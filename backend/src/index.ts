7a5f899099bb7749d115db5d1570f00ef2a39ceb
Abhishek <sreenivas1013@gmail.com>
2026-03-12 14:12:29 +0530

    done broadcast messaging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,32 @@
-console.log("Nice");
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ['GET', 'POST'],
+    }
+})
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+io.on("connection", (socket) => {
+    console.log("User connected", socket.id);
+    socket.on("message", (message) => {
+        console.log(`You sent ${message}`);
+        socket.send(`sent ${message}`);
+    })
+
+    socket.on("chat", (message) => {
+        socket.broadcast.emit("chat", message);
+    })
+})
+
+httpServer.listen(3000, () => {
+    console.log("Server running on port 3000");
+})
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,33 +3,61 @@ import { Sidebar } from './components/Sidebar';
 import { ChatArea } from './components/ChatArea';
 import { CONTACTS, INITIAL_MESSAGES } from './data/mockData';
 import type { Message } from './types/chat';
+import { io, Socket } from 'socket.io-client';
+import { v4 as uuidv4 } from 'uuid';
+import { useEffect, useRef } from 'react';
 
 function App() {
+
+  const socketRef = useRef<Socket | null>(null);
+
+  useEffect(() => {
+    socketRef.current = io("http://localhost:3000");
+
+    socketRef.current.on("connect", () => {
+      console.log("Connected with id", socketRef.current?.id);
+    })
+
+    socketRef.current.on("disconnect", () => {
+      console.log(`Socket disconencted`);
+    })
+
+    return (() => {
+      socketRef.current?.disconnect();
+    })
+  }, [])
+
   const [activeContactId, setActiveContactId] = useState<number | string>(CONTACTS[0].id);
   const [messages, setMessages] = useState<Message[]>(INITIAL_MESSAGES);
 
   const activeContact = CONTACTS.find(c => c.id === activeContactId) || CONTACTS[0];
 
+
+  useEffect(() => {
+    if (!socketRef.current) return;
+
+    socketRef.current.on("chat", (reply: string) => {
+      const replyMessage: Message = {
+        id: uuidv4(),
+        senderId: 'friend',
+        text: reply,
+        timestamp: new Date()
+      };
+
+      setMessages(prev => [...prev, replyMessage]);
+    })
+  }, [])
+
   const handleSendMessage = (text: string) => {
+    socketRef.current?.emit("chat", text);
     const newMessage: Message = {
-      id: messages.length + 1,
+      id: uuidv4(),
       senderId: 'me',
       text,
       timestamp: new Date()
     };
 
     setMessages(prev => [...prev, newMessage]);
-
-    // Simulate reply
-    setTimeout(() => {
-      const replyMessage: Message = {
-        id: messages.length + 2,
-        senderId: activeContact.id,
-        text: 'That looks amazing! Keep up the good work. 🚀',
-        timestamp: new Date()
-      };
-      setMessages(prev => [...prev, replyMessage]);
-    }, 1500);
   };
 
   return (
